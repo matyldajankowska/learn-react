27f9ce5e3f9fbb0ba89d2731f02102dd75a0f8b1
Matylda Jankowska <matylda.jankowska@hotmail.com>
2021-04-08 07:47:12 +0200

    add props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,12 +7,13 @@ class App extends React.Component {
   render() {
     return (
       <main className={styles.component}>
-        <h1 className={styles.title}>My first React app</h1>
-        <h2 className={styles.subtitle}>Hello world!</h2>
-        <List />
-      </main>
-    )
-  }
+      <h1 className={styles.title}>My first React app</h1>
+      <h2 className={styles.subtitle}>Hello world!</h2>
+      <List>
+     </List>
+    </main>
+  )
+}
 }
 
 export default App;
