37aa88ae6d4c97cd09081dd64605aad8429563dc
Matylda Jankowska <matylda.jankowska@hotmail.com>
2021-04-09 08:13:50 +0200

    add html parser

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './Hero.scss';
 import PropTypes from 'prop-types';
+import ReactHtmlParser from 'react-html-parser';
 
 const Hero = props => (
         <header className={styles.component}>
-            <h2 className={styles.title}>{props.titleText}</h2>
+            <h2 className={styles.title}>{ReactHtmlParser(props.titleText)}</h2>
             <img src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" className={styles.image}></img>
         </header>
     );
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,22 +3,27 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero.js'
 import PropTypes from 'prop-types';
 import Column from './../Column/Column.js';
+import {settings} from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
 
 class List extends React.Component {
   static propTypes = {
     title: PropTypes.node,
-    children: PropTypes.node,
+    description: PropTypes.node,
+    columns: PropTypes.array,
   };
+
   static defaultProps = {
-    children: <p>I can do all the things!!!</p>,
+    description: settings.defaultListDescription, 
   };
+
   render() {
     return (
       <section className={styles.component}>
         <h2><Hero titleText={this.props.title} heroImage={this.props.image}/>
         </h2>
         <div className={styles.description}>
-          {this.props.children};
+        {ReactHtmlParser(this.props.description)};
         </div>
         <div className={styles.columns}>
           <Column titleColumn={'Animals'}> 
